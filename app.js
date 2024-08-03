404ce78d54f66d3518ed07d5a23a7d05ede08ee2
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-08-03 21:07:01 +0200

    env settings

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import dotenv from "dotenv/config";
 import sequelize from "./db/sequelize.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
