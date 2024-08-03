b97e6aadf7e91567341905c61bcd685bd47420e3
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-08-03 20:26:35 +0200

    add config to sequelize

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,5 +1,25 @@
 import { Sequelize } from "sequelize";
-import { databaseConfig } from "./config.js";
+
+const {
+  DATABASE_DIALECT,
+  DATABASE_USERNAME,
+  DATABASE_NAME,
+  DATABASE_PASSWORD,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = process.env;
+
+const databaseConfig = {
+  dialect: DATABASE_DIALECT,
+  username: DATABASE_USERNAME,
+  database: DATABASE_NAME,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  port: DATABASE_PORT,
+  dialectOptions: {
+    ssl: true,
+  },
+};
 
 const sequelize = new Sequelize(databaseConfig);
 
