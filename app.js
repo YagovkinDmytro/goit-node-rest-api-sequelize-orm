a6b2873e4df8ba1fe4692dfc4812514c95d200fd
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-08-03 19:01:04 +0200

    create sequelize

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import sequelize from "./db/sequelize.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
@@ -20,6 +20,13 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
 
 app.listen(3000, () => {
   console.log("Server is running. Use our API on port: 3000");
