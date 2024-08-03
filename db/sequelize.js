ec10510faa7df1c25dd4ecec6cb41daaf15f7a9f
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-08-03 19:37:48 +0200

    add database config to config.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,15 +1,6 @@
 import { Sequelize } from "sequelize";
+import { databaseConfig } from "./config";
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  username: "dmytro",
-  database: "db_contacts_b5nk",
-  password: "h8RIkGWjyis134ADMP3E6bLc1BQlFPUU",
-  host: "dpg-cqmdmt08fa8c73cgudug-a.frankfurt-postgres.render.com",
-  port: "5432",
-  dialectOptions: {
-    ssl: true,
-  },
-});
+const sequelize = new Sequelize(databaseConfig);
 
 export default sequelize;
